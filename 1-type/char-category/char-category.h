40375c664c70af2dfb6ee3cc94bf75ec61f8eba0
sanya anakonda <borodsh@yandex.ru>
2023-10-22 17:32:34 +0300

    task 1

--- 1-type/char-category/char-category.h
+++ 1-type/char-category/char-category.h
@@ -2,18 +2,50 @@
 
 
 bool is_digit(char x) {
-    return true; // No implementation
+    int b;
+    b = (int)x;
+    if (b >= 48 && b <= 57){
+    return true;
+    }
+    else{
+    return 0;
+    }
 }
 
 bool is_letter(char x) {
-    return true; // No implementation
+    int b;
+    b = (int)x;
+    if ((b >= 65 && b <= 90) || (b >=97 && b <= 122)){
+    return true;
+    }
+    else{
+    return 0;
+    }
 }
 
 bool is_punctuation(char x) {
-    return true; // No implementation
+    int b;
+    b = (int)x;
+    if ((b >= 33 && b <= 47) || (b >= 58 && b <= 64) || (b >= 91 && b <= 96) || (b >= 123 && b <=126)){
+    return true;
+    }
+    else{
+    return 0;
+    }
 }
 
 int get_ascii_code(char first, char second, char third) {
-    return 0; // No implementation
+    int fir, sec, thir;
+    fir = (int)first;
+
+    sec = (int)second;
+
+
+    thir = (int)third;
+    int sum;
+    sum = (fir * 1000000) + (sec * 1000) + thir;
+
+    return (sum);
+
 }
 
--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -1,3 +1,13 @@
 int get_time(long long angle) {
-    return 0; // Not implementation
+    int hour, minutes, sec;
+    hour = angle / (21600);
+    int angle1;
+    
+    angle1 = angle - hour * 21600;
+    
+    minutes = (angle1 / 360);
+    sec = (angle - (hour * 21600) - minutes * 360) / 6;
+    int sum;
+    sum = hour * 1000 + minutes *100 + sec;
+    return sum;
 }
