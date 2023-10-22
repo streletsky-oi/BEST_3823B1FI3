c5ce566c72cd87769817966c5e568079348cf5bc
sanya anakonda <borodsh@yandex.ru>
2023-10-22 22:24:35 +0300

    we

--- 1-type/byte/byte.h
+++ 1-type/byte/byte.h
@@ -1,3 +1,17 @@
 unsigned char max_byte(unsigned short number) {
-    return 0; // Not implementation
+    unsigned char maxByte = 0;
+
+    // Разделяем переменную на два байта
+    unsigned char byte1 = number & 0xFF;
+    unsigned char byte2 = (number >> 8) & 0xFF;
+
+    // Сравниваем байты и находим максимальный
+    if (byte1 > maxByte) {
+        maxByte = byte1;
+    }
+    if (byte2 > maxByte) {
+        maxByte = byte2;
+    }
+
+    return maxByte;
 }
--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -1,3 +1,15 @@
 int get_score(short score, int start, int stop, int now) {
-    return 0; // Not implementation
+    if (now <= start){
+        return(score);
+    }
+    else if(now < stop){
+        double newscore;
+        newscore == round(score - (score / (stop - start)) * (now - start)); 
+        return newscore;
+    }
+
+    else{
+        score = score / 2;
+        return(score);
+    }
 }
