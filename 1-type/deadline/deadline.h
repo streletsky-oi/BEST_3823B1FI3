d97dca88c5f0e4484a7b3dad5d8a8177d94d4aca
sanya anakonda <borodsh@yandex.ru>
2023-10-22 23:10:31 +0300

    sd

--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -1,15 +1,14 @@
 int get_score(short score, int start, int stop, int now) {
-    if (now <= start){
-        return(score);
-    }
-    else if(now < stop){
-        double newscore;
-        newscore == round(score - (score / (stop - start)) * (now - start)); 
-        return newscore;
-    }
-
-    else{
-        score = score / 2;
-        return(score);
-    }
+      if (now <= start){
+        return score;}
+    if (now >= stop){
+        return (score+1)/2;}
+    if ((now > start) && (now < stop)){
+        float res,score_half;
+        int ans, res_int;
+        score_half = score/2;
+        res = score_half / (stop - start) * (now - start) + 0.1;
+        res_int = res; 
+        ans = score - res_int;
+        return ans;}  
 }
