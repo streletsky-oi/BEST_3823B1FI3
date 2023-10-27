ef116fe2ac478e5e3705071476c32470be375998
sanya anakonda <borodsh@yandex.ru>
2023-10-27 19:15:22 +0300

    ifelse

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,3 +1,6 @@
 long long change(int a, int b, int c, int d){
-    return 0;
+    long long u;
+    u = ((long long)c * 100 + (long long)d) - ((long long)a * 100 + (long long)b);
+    if (u >= 0) return u;
+    return -1;
 }
