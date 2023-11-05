cdc90b70f7f6f9e486171b264b99a47c166ad9ba
sanya anakonda <borodsh@yandex.ru>
2023-11-05 21:30:48 +0300

    loop1

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,27 @@
 #include <math.h>
 
 char find_bin_pattern(int number){
-    return 100;
+    int n = number;
+    
+    int binaryNum[1000]; 
+  
+    int count = 0;
+    int i = 0; 
+    while (n > 0) { 
+        // storing remainder in binary array 
+        binaryNum[i] = n % 2; 
+        n = n / 2; 
+        i++; 
+    }
+    
+        
+    for (int j = i - 1; j >= 2; j--){
+        if (binaryNum[j] == 1 && binaryNum[j-1] == 0 && binaryNum[j-2] == 1){
+        
+            count += 1;
+        }
+        
+    }
+    
+    return count;
 }
--- 3-loop/count_max_digit/count_max_digit.h
+++ 3-loop/count_max_digit/count_max_digit.h
@@ -1,5 +1,52 @@
 #include <math.h>
 
-char count_max_digit(long long int){
-    return 100;
+char count_max_digit(long long int n){
+    
+    
+    
+    if (n < 0){
+        n = n * -1;
+    }
+    long long int cnt = 1; //забыл как тру делать
+    int N = 0; //счетчик для подсчитывания кол-во цфир в числе
+    long long tt, mn = 10;
+    while(cnt != 0){ //делим постоянно на 10, 100, 1000 и тд пока не получим ноль
+        tt = n / mn;
+        if (tt == 0){
+            break;
+        }
+        mn *= 10;
+        N += 1;
+    }
+    N = N +1;
+    
+    long long int max;
+    max = 0;
+    long long int t; // цифра 
+    long long int del = 10; // для деления - отбросить числа справа
+    long long int ch = 1; // для остатка - отбрсоить числа слева
+    for (int i = 1; i <= N; i++){
+        t = (n % del) / ch; //выделяем цифру путем откидывания чисел справа/слева 
+        if (t > max){
+            max = t;
+        }
+        
+        del = del * 10;
+        ch = ch * 10;
+    }
+    
+    
+    char cntt = 0; //еще один счетчик макс цифер
+    del = 10;
+    ch = 1;
+    for (int i = 1; i <= N; i++){
+        t = (n % del) / ch;
+        
+        if (t == max){
+            cntt = cntt + 1;
+        }
+        del = del * 10;
+        ch = ch * 10;
+    }  
+    return cntt;
 }
