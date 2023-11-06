1230355287638f4705080c0c1f863eafb6eb4609
sanya anakonda <borodsh@yandex.ru>
2023-11-06 23:10:35 +0300

    loop

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -2,7 +2,23 @@
 
 int find_elem(int left, int right, int number, int max_iter){
     int i = 0; //Счётчик итераций
-
+    int dig = number, oper = max_iter;
+    int trueleft = left;
+    if (dig < left || dig > right){
+        return -1;
+    }
+    while (left <= right && i <=oper){
+        i +=1;
+        int middle = (left + right) / 2;
+        if (dig == middle){
+            return i * (middle - trueleft);
+            break;
+        }
+        else if (dig < middle)
+            right = middle - 1;
+        else 
+            left = middle + 1;
+    }
 
     return i; //НЕ МЕНЯТЬ!
 }
