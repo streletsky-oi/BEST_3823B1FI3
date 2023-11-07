c020b59c54f93177396a400a193e053d49de01ed
sanya anakonda <borodsh@yandex.ru>
2023-11-08 01:58:45 +0300

    dsa

--- 4-array/get_percent/calc_percent.h
+++ 4-array/get_percent/calc_percent.h
@@ -1,5 +1,26 @@
 #include <math.h>
 
 int calc_percent(int matrix[], int size){
-    return 0;
+    int min = matrix[0], max = matrix[0];
+    
+    for (int i = 0; i < size; i++){
+        if(matrix[i] > max){
+            max = matrix[i];
+        }
+        if(matrix[i] < min){
+            min = matrix[i];
+        }
+            
+    }
+    int r;
+    r = (max - min) / 2;
+    float sizef = (float)size;
+    float count = 0.0;
+    for (int j = 0; j < size; j++){
+        if (matrix[j] > r){
+            count += 1;
+        }
+            
+    }
+    return (count / sizef) * 100;
 }
