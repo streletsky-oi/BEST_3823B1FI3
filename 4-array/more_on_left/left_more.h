b5f25fa0cc75a7e23ef6004553d3a187d7a6c21f
sanya anakonda <borodsh@yandex.ru>
2023-11-08 02:07:24 +0300

    awe

--- 4-array/more_on_left/left_more.h
+++ 4-array/more_on_left/left_more.h
@@ -1,3 +1,9 @@
 char left_more(double input[], unsigned int size){
-    return -1;
+    int count = 0;
+    for(int i = 1; i < size; i++){
+        if (input[i] > input[i - 1]){
+            count += 1;
+        }
+    }
+    return count;
 }
