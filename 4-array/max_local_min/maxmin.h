b2fd3b3dc14aecf464e2b4a213350e8f6232100a
sanya anakonda <borodsh@yandex.ru>
2023-11-09 02:31:02 +0300

    arrays

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -1,3 +1,29 @@
 int maxmin(int input[], int size){
-    return -1;
+   
+    int count = 0; // размер используемого хранилища минимумов 
+    int locmins[100]; // массив для хранения минимумов, 
+    int locmin;
+    if (input[0] < input[1]){ //обрабатываем первый член и последний как исключение
+        locmin = input[0];
+        locmins[count] = locmin;
+        count +=1;
+    }
+    for (int i = 1; i < size-1; i ++){
+        if(input[i] < input[i -1] && input[i] < input[i+1]){
+            locmin = input[i];
+            locmins[count] = locmin;
+            count += 1;
+        }
+    }
+    if (input[size -1] < input[size -2]){ //обрабатываем первый член и последний как исключение
+        locmin = input[size -1];
+        locmins[count] = locmin;
+    }
+    int max = locmins[0];
+    for (int j = 0; j < count; j++){
+        if(locmins[j] < locmins[j+1]){
+            max = locmins[j+1];
+        }
+    }
+    return max;
 }
