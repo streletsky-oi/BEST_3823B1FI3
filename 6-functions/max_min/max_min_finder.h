153a79b3a9855389a15b8b1218f79c4e80d4fa12
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-17 23:39:45 +0300

    Update max_min_finder.h

--- 6-functions/max_min/max_min_finder.h
+++ 6-functions/max_min/max_min_finder.h
@@ -1,5 +1,29 @@
 #include <math.h>
+int minimal(int *arr, int size, int *mn){
+    for (int i = 0; i < size; i++){
+        if(*(arr + i) < *mn){
+            *mn = *(arr + i);
+        }
+    }
+    return(*mn);
+}
+
+int maximal(int *arr, int size, int *mx){
+    for (int i = 0; i < size; i++){
+        if(*(arr + i) > *mx){
+            *mx = *(arr + i);
+        }
+    }
+    return(*mx);
+}
 
 void get_max_min(int* array, int size, int* pMax, int* pMin){
-    return;
+    *pMin = *array;
+    *pMax = *array;
+    *pMax = maximal(array, size, pMax);
+    *pMin = minimal(array, size, pMin);
+    return(pMax);
+    return(pMin);
+    
+    
 }
