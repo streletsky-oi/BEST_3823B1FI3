5ab01d2104a2f3aa079bded9b277f6ee1c67cd83
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-17 23:42:30 +0300

    Update pack.h

--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,20 +1,24 @@
-#include <math.h>
+
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-    
-
+    unsigned long long l = 0;
+    for (int i = 0; i < size; i++) {
+        if (size > 8){
+            return 0;
+            break;
+        }
+        l = l | ((unsigned long long)arr[i] << (8 * i));
+    }
+    return l;
 }
 
 unsigned char unpack_ull(unsigned long long input){
     unsigned long long n = input;
-    
-    
-    for (int i = 0; i < 8; i++){
-        char a = (char)n;
+    char a;
+    for (int i = 0; i < 5; i++){
+        a = (char)n;
         n = n >> 8;
-        if (i == 4){
-            return a;
-        }
         
     }
+    return a;
 }
