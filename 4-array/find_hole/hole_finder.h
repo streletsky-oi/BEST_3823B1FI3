588acf86c38997cdb5ca78e7771f08c2a4fe0e3c
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-17 23:43:37 +0300

    Update hole_finder.h

--- 4-array/find_hole/hole_finder.h
+++ 4-array/find_hole/hole_finder.h
@@ -1,5 +1,16 @@
-#include <math.h>
 
+#include <stdio.h>
 int hole_finder(int matrix[], int length, int width){
-    return 0;
+    int min = 99999;
+    int x = 0;
+    int j = 0;
+    for (int i = 0; i<length*width; i++){
+        if (matrix[i]<min){
+            x = i/width;
+            j = i-(x*width);
+            min = matrix[i];
+        }
+    }
+    return x*100+j;
+    //cmm!!!
 }
