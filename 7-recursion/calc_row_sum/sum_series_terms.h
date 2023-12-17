bb4258deb8061e68fe76fdfdbf7dc3578728a55e
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-17 23:35:40 +0300

    Update sum_series_terms.h

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,5 +1,10 @@
 #include <math.h>
-
+double recurs(int n){
+    if(n == 1) return n;
+    return (double)1/n + (double)recurs(n-1);
+}
 double sum_series(int n){
-    return -1.0;
+    double k;
+    k = recurs(n);
+    return(k);
 }
