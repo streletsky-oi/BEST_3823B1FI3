79b111a90e77e4a6b60709f0b97de8a7ab4708a7
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-17 23:41:57 +0300

    Update find_even.h

--- 5-pointer/even/find_even.h
+++ 5-pointer/even/find_even.h
@@ -1,5 +1,21 @@
 #include <math.h>
 
 int* find_even(int* arr, int size){
-    return 0;
+    
+    
+    
+    int i;
+    
+    for (i = 0; i < size; i++){
+        if(*(arr+i) % 2 == 0){
+            
+           
+            return (arr+i);
+            
+            break;
+        }
+    }
+    if(*(arr + i) % 2 != 0){
+        return NULL;
+    } //cmm!!
 }
