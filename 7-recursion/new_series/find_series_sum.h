0c73d5cea6a077fa415e8cdc5d2bd4fee7dd7d78
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-17 23:36:17 +0300

    Update find_series_sum.h

--- 7-recursion/new_series/find_series_sum.h
+++ 7-recursion/new_series/find_series_sum.h
@@ -1,5 +1,21 @@
 #include <math.h>
+double raw(int n){
+    if(n == 1 || n == 0){
+        
+        return 1;
+    }
+    
+    return (raw(n-1) + 1) / (n);
 
+}
 double find_sum_elements_series(int k){
-    return -1.0;
+    
+    double sum = 0;
+    
+    
+    for (int i = 1; i <= k; i++){
+        sum += raw(i);
+    }
+    
+    return sum;
 }
