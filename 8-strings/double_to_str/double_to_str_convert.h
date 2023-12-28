37ac8dd23ff9fd294efff6eba160f3359f884eb6
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-29 01:01:52 +0300

    Update double_to_str_convert.h

--- 8-strings/double_to_str/double_to_str_convert.h
+++ 8-strings/double_to_str/double_to_str_convert.h
@@ -1,6 +1,18 @@
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
+char* floatToString(double number)
+{
+    char buffer[256];
+    snprintf(buffer, sizeof(buffer), "%f", number);
+    return buffer;
+}
 
 char* convert(double number){
-    return NULL;
+    
+    char* string = floatToString(number);
+
+    
+
+    return string;
 }
