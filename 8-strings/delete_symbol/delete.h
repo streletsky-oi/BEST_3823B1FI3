bc7beda3d1bd504de5037967a0d62a1bb32ec454
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-29 01:01:29 +0300

    Update delete.h

--- 8-strings/delete_symbol/delete.h
+++ 8-strings/delete_symbol/delete.h
@@ -2,6 +2,30 @@
 #include <string.h>
 
 void DelSymbol1(char *str, char sym){
+    
+    
+    int len;
+    len = strlen(str);
+    int i = 0;
+    char s[1];
+    while(*(str + i) != 0){
+        
+        
+        s[0] = *(str + i);
+        if(s[0] == sym){
+            
+            len--; // размер массива
+            for(int j = i; j < len; j++){
+                *(str + j) = *(str + j + 1);
+            }
+        }
+        
+        if(*(str + i) != sym){
+            i++;
+        }
+        
+    }
 
+    return(str); // выводит строку прошлого размера
+    
 }
-
