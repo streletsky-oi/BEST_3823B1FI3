01952dff517afa201538fcd833b94e836d680f57
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-29 01:08:47 +0300

    Update lower_converter.h

--- 8-strings/to_lower/lower_converter.h
+++ 8-strings/to_lower/lower_converter.h
@@ -1,6 +1,9 @@
-#include <math.h>
-#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
-void lower(char* str){
-
+void lower(char* str)
+{
+    for (; *str != '\0'; str++) {
+        *str = tolower((unsigned char) *str);
+    }
 }
