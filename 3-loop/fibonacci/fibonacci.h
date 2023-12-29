b6f5cf42b3188ac7abccd5509dcef521bb597f0b
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2023-12-29 11:41:14 +0300

    Update fibonacci.h

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,26 +1,17 @@
-int fibonaci(long long k){
-    long long int f0 = 0, f1 = 1, fk = 0, sum = 0;
-    if (k < 0){
-        return -1;
-    }
-    else{
-        while(fk < k){
-            fk = f0 + f1;
-            if (fk > k){
-                break;
-            }
-            sum += fk;
-            f0 = f1;
-            f1 = fk;
-        }
-        return sum + 1;
-
-    }
-    
-}
-
-
 long fib(int m){
-    fibonaci(m);
-    
+    int sum = 1, prev = 1, curr = 1, tmp;
+
+    if (m < 0)
+        return -1;
+        
+    while (curr <= m)
+    {
+        sum += curr;
+        tmp = curr;
+        curr += prev;
+        prev = tmp;
+    }
+
+    return sum;
+
 }
